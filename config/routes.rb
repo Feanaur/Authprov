83ebd1d67500ddcbf3059a2bcb6eae02131c9333
Feanaur <feanaur2193@gmail.com>
2014-01-27 19:53:49 +0700

    Welcome destoyed. Bootstrap and icon links added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Authprov::Application.routes.draw do
   get "users/index"
   get "users/profile"
-  get "welcome/index"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users, :only => [:destroy, :index]
   # The priority is based upon order of creation: first created -> highest priority.
