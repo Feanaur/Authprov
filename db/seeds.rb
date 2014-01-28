e0270d673680b4661de77515da9b7a7b98e115c0
Feanaur <feanaur2193@gmail.com>
2014-01-28 12:35:55 +0700

    Devise friendly token added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create( provider: "vkontakte",
+                    uid: "18113494",
+                    name: "Александр Колмаков",
+                    email: 'id18113494@vk.com',
+                    image: "http://cs864.vk.me/u18113494/e_ef6105de.jpg",
+                    password: Devise.friendly_token[0,20],
+                    roles: [:admin])
+user.save
