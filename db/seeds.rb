4455597953ac64b76abc7c7987ee26a5d840e5b0
Feanaur <feanaur2193@gmail.com>
2014-01-28 12:48:23 +0700

    Seed for facebook aded

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@ user = User.create( provider: "vkontakte",
                     password: Devise.friendly_token[0,20],
                     roles: [:admin])
 user.save
+user = User.create( provider: "facebook",
+                    uid: "100003281644312",
+                    name: "Alexander Kolmakov",
+                    email: 'feanaur2193@gmail.com',
+                    image: "http://graph.facebook.com/Feanaur/picture",
+                    password: Devise.friendly_token[0,20],
+                    roles: [:admin])
+user.save
